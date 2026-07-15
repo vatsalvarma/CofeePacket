abe47b2d1b3a8bd76197225e2c935719215c8fe4
Vatsal varma <gamesvatsal99@gmail.com>
2026-07-15 17:37:38 +0530

    update

--- animations/vite.config.ts
+++ animations/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DinnerPlate-Animation/',
+  base: '/CofeePacket/',
   plugins: [
     tailwindcss(),
     react()
